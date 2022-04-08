cee6314d740d89c7d85669c02ef7809f55063674
Roman Fedorov <rfedorov@playtika.com>
2022-04-08 20:52:08 +0300

    added test event raise extension

--- RestApiOnCore.Unit.Tests/ServicesTests.cs
+++ RestApiOnCore.Unit.Tests/ServicesTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
@@ -6,10 +7,42 @@ using RestApiOnCore.Services.Instrastucture;
 
 namespace RestApiOnCore.Unit.Tests;
 
+public static class EventExtensions
+{
+	public static void Raise(this object instance, string eventName, EventArgs eventArgs = null)
+	{
+		const BindingFlags staticFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+		var type = instance.GetType();
+		var eventField = type.GetField(eventName, staticFlags);
+
+		if (string.IsNullOrEmpty(eventName))
+		{
+			throw new Exception($"Event with name {eventName} could not be found.");
+		}
+
+		var multicastDelegate = eventField.GetValue(instance) as Delegate;
+
+		if (eventArgs != null)
+		{
+			multicastDelegate.DynamicInvoke(eventArgs);
+		}
+		else
+		{
+			multicastDelegate.DynamicInvoke();
+		}
+	}
+}
+
 [TestFixture]
 public class ServicesTests
 {
 	private IDateTimeService _dateTimeService;
+
+	public class SomeCLassWithEvent
+	{
+		public event System.Action CloseClick;
+	}
+
 
 	[SetUp]
 	public void Setup()
@@ -26,5 +59,16 @@ public class ServicesTests
 		var savedTime = new DateTime(2022, 3, 14);
 		var diff = _dateTimeService.GetDateTimeNow() - savedTime;
 		diff.TotalDays.Should().BeLessOrEqualTo(3);
+	}
+
+	[Test]
+	public void TestRaiseEvent()
+	{
+		var savedTime = new SomeCLassWithEvent();
+		bool clicked = false;
+		savedTime.CloseClick += () => { clicked = true; };
+		savedTime.Raise(nameof(SomeCLassWithEvent.CloseClick));
+
+		clicked.Should().BeTrue();
 	}
 }
